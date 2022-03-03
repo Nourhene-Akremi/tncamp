926752acf3ee9019501f2ec8446e0e60a71dc5ba
unknown <akreminourhene@gmail.com>
2022-03-03 17:47:20 +0100

    backend:create  sigup with success

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,10 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const auth = express.Router()
-const userSchema=require('../models/userModels')
+const userSchema = require('../models/userModels')
 //*****the form uses post methode to send request to the db server*** we use post methode because it contain a body when we will send the sensetive information that we wanted to anyone to see it in the url **for the information safety and security we uses methode POST***/
 //we uses the parameter req and res to storage the request that we will sennd it  and to storage the answer the respond send it by the server ***//
-auth.post('/signup',async(req,resp)=>{
+auth.post('/signup', async(req,resp)=>{
     //distract data to avoid the repetion of req.body
     const{photo,name,email,password,phone_number,adress,role}=req.body
     
@@ -13,18 +13,18 @@ auth.post('/signup',async(req,resp)=>{
     try {
         //**because express is asynchronous we need to use the await keyword that force the waiting of the specified iteration 
         //asynchronus programm is the one who ignore the first step because of the delay and dispatch the next one and that can cause many execution problems**//
-     
         const find = await userSchema.findOne({email:email})
       if (find) {
-          resp.status(400).send({message:'user already exist'})
-      }
-        const user=new userSchema(req,resp)
+          resp.status(400).send({message:'user already exist'})}
+        const user= new userSchema(req.body)
         const salt=10
         const passwordbhashed=bcrypt.hashSync(password,salt)
-        user.password=passwordbhashed
-
+        const userId={id:user._id}
+        var token = jwt.sign(userId,process.env.SECRET_OR_KEY)
+        user.password = passwordbhashed
         //create the user in the data base
         await user.save()
+        resp.status(200).send({message:'user added with success',token})
     } catch (error) {
         resp.status(400).send({message:'failed to add user'})
     }
