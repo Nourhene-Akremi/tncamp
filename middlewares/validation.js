b3ab5d688f7e9713f1bf09a8bd879b46374c068d
unknown <akreminourhene@gmail.com>
2022-03-05 19:38:50 +0100

    middleware dosent work VALIDATION

---
+++ middlewares/validation.js
@@ -0,0 +1,27 @@
+const {body,validationResult}=require('express-validator')
+
+exports.validationSignUp=[
+    body("photo"),
+    body("name","name can not be empty and at least contain five caracters").isEmpty().isLength({min:5}),
+    body("email","please type a correct email").isEmpty().isEmail,
+    body("password","password can not be empty and have to contain at least five character").isEmpty().isLength({min:5}).isAlphanumeric(),
+    body("phone_number","please type a correct phone number").isEmpty(),
+    body("adress","adresse can not be empty and have to contain at least ten caracters").isEmpty().isLength({min:10})
+]
+
+exports.validationSignIn=[
+    body("email","please type a correct email").isEmpty().isEmail,
+    body("password","password can not be empty and have to contain at least five character").isEmpty().isLength({min:5}).isAlphanumeric()
+]
+
+exports.isValid= async(req,resp,next) => {
+    try {
+       const errors = validationResult(req)
+       if(!errors.isEmpty())
+       {
+           return resp.status(400).json({errors:errors.array() })};
+       next();
+    } catch (error) {
+        resp.status(400).send({message:error}) 
+    }
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,61 +1,11 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const auth = express.Router()
-const userSchema = require('../models/userModels')
-//SIGNUP
+const {authSignup,authSignin}=require('../controllers/userController')
 //*****the form uses post methode to send request to the db server*** we use post methode because it contain a body when we will send the sensetive information that we wanted to anyone to see it in the url **for the information safety and security we uses methode POST***/
 //we uses the parameter req and res to storage the request that we will sennd it  and to storage the answer the respond send it by the server ***//
-auth.post('/signup', async(req,resp)=>{
-    //distract data to avoid the repetion of req.body
-    const{photo,name,email,password,phone_number,adress,role}=req.body
+const {validationSignUp,validationSignIn,isValid}=require('../middlewares/validation')
+auth.post('/signup',validationSignUp,isValid,authSignup )
+
+auth.post('/signin',validationSignIn,isValid,authSignin)
     
-//****we use try and catch statement like we use the if and else statement to verify the res and to know their type by the status code and by the message *****//
-    try {
-        //**because express is asynchronous we need to use the await keyword that force the waiting of the specified iteration 
-        //asynchronus programm is the one who ignore the first step because of the delay and dispatch the next one and that can cause many execution problems**//
-        const find = await userSchema.findOne({email:email})
-      if (find) {
-          resp.status(400).send({message:'user already exist'})}
-        const user= new userSchema(req.body)
-        const salt=10
-        const passwordbhashed=bcrypt.hashSync(password,salt)
-        const userId={id:user._id}
-        const token = jwt.sign(userId,process.env.SECRET_OR_KEY)
-        user.password = passwordbhashed
-        //create the user in the data base
-        await user.save()
-        resp.status(200).send({message:'user added with success',token})
-    } catch (error) {
-        resp.status(400).send({message:'failed to add user'})
-    }
-})
-//SIGNIN
-
-auth.post('/signin',async(req,resp)=>{
-    const{email,password}=req.body
-    try {
-        const find = await userSchema.findOne({email:email})
-        if (!find) {
-        resp.status(400).send({message:'bad credentials'})}
-        const match= await bcrypt.compare(password,find.password)
-        if(!match)
-        {resp.status(400).send({message:'bad credentials'})}
-        const userId = {id:find._id}
-        const token = jwt.sign(userId,process.env.SECRET_OR_KEY)
-        resp.status(200).send({message:'login successfully',token})
-    } catch (error) {
-        resp.status(400).send({message:'bad credentials'})
- 
-    }
-})
-    
-
-
-
-
-
-
-
-
 module.exports=auth;
